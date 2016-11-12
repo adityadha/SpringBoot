e670250cbd214e581749bd39db1f9da0f08ee9c8
Aditya Dhamanar <adityadh@INadityadh.local>
2016-11-12 17:50:08 +0530

    Added test

---
+++ src/test/java/com/tw/bootcamp/chennai/domain/EmployeeRecordTest.java
@@ -0,0 +1,12 @@
+package com.tw.bootcamp.chennai.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EmployeeRecordTest {
+    @Test
+    public void testSample(){
+        assertTrue(true);
+    }
+}
