6aa1020401e6af1e86605490ed8074ed96ed6d99
Aditya Dhamanar <adityadh@INadityadh.local>
2016-11-12 17:59:56 +0530

    test

--- src/test/java/com/tw/bootcamp/chennai/domain/EmployeeRecordTest.java
+++ src/test/java/com/tw/bootcamp/chennai/domain/EmployeeRecordTest.java
@@ -2,11 +2,13 @@ package com.tw.bootcamp.chennai.domain;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class EmployeeRecordTest {
     @Test
     public void testSample(){
         assertTrue(true);
+        assertFalse(false);
     }
 }
